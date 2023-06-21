117d65a6027414defba8628c1257263ce87797d3
JabulisileJuanah <maphangajabulisile@gmail.com>
2023-06-20 23:42:11 -0700

    10. a + b

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * print _times_table - prints te n times table, starting with 0
@@ -20,11 +20,11 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 					_putchar(',');
-				        _putchar(' ');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-			} else if (k >= 10 && k < 100)
+			} if (k >= 10 && k < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,20 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
  */
-int _abs(int)
+
+int _abs(int c)
+
 {
-	if  (n >= 0)
+
+	if (c < 0)
 	{
-		return (n);
+	int _abs_val;
+
+	_abs_val = c * -1;
+	return (abs_val);
 	}
-	return (-n);
+	return (c);
 }
