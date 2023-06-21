038d3d240156bf1c7edadc308697b291f4050cc8
JabulisileJuanah <maphangajabulisile@gmail.com>
2023-06-21 09:33:16 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,20 +2,16 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @c: The number to be computed.
- * Return: Absolute value of number or zero
+ * @n: the int to check
+ * Return: the absolute value of int
  */
 
-int _abs(int c)
-
+int _abs(int n)
 {
 
-	if (c < 0)
+	if (n < 0)
 	{
-	int _abs_val;
-
-	_abs_val = c * -1;
-	return (abs_val);
+	return (n);
 	}
-	return (c);
+	return (-n);
 }
