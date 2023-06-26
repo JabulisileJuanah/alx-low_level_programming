844d53acd2ff88398014b20698d2466e2848a34b
JabulisileJuanah <maphangajabulisile@gmail.com>
2023-06-26 12:44:02 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,11 +10,10 @@ int _atoi(char *s)
 {
 int i, d, n, len, f, digit;
 
-i = ("98");
+i = 0;
 d = 0;
 n = 0;
 len = 0;
-f = 0;
 digit = 0;
 
 while (s[len] != '\0')
@@ -24,6 +23,7 @@ while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
@@ -37,8 +37,6 @@ f = 0;
 }
 i++;
 }
-
-if (f == 0)
 return (0);
 
 return (n);
